a87f16215fbad0615ab5ce35d30519c082902217
Walid <walid_fekry@hotmail.com>
2025-11-17 05:41:33 +0200

    Edit header.php

--- includes/header.php
+++ includes/header.php
@@ -55,6 +55,16 @@ $structuredData = $structuredData ?? '';
     <script>
         const BASEURL = '<?= BASE_URL ?>';
     </script>
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-6XFFFR8JXF"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+
+        gtag('config', 'G-6XFFFR8JXF');
+    </script>
 </head>
 
 <body>
